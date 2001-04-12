85d9a72443d43d087116c98245659e44ace9ae3d
agillesp <agillesp@ee1dc550-b172-11e0-879d-2b1ccd377a6e>
2001-04-12 14:50:31 +0000

    Added LGPL Headers to all source files, plus the full text of LGPL in LICENSE file
    git-svn-id: file:///home/akrueger/3rdParty/freehost3270-svn/trunk@6 ee1dc550-b172-11e0-879d-2b1ccd377a6e

--- freehost3270/src/com/ino/freehost/client/RWTelnet.java
+++ freehost3270/src/com/ino/freehost/client/RWTelnet.java
@@ -1,4 +1,28 @@
 /*
+ * Freehost3270 - A web deployment system for TN3270 clients
+ *
+ * Copyright (C) 1998,2001 Art Gillespie
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * The Author can be contacted at agillesp@i-no.com or
+ * 185 Captain Whitney Road (Becket)
+ * Chester, MA  01011
+ */
+
+/*
  * 5/12/98 - Added connect(String host, int port) to facilitate
  * the 3270 Servlet Development Toolkit.  This method enables
  * implementations to connect directly to a TN3270 host
