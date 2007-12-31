e7bffaf606094fb00c8b3ed6746992d67887f7b5
jstrayer <jstrayer@ee1dc550-b172-11e0-879d-2b1ccd377a6e>
2007-12-31 18:10:46 +0000

    Added a main method so it can run standalone
    git-svn-id: file:///home/akrueger/3rdParty/freehost3270-svn/trunk@87 ee1dc550-b172-11e0-879d-2b1ccd377a6e

--- freehost3270/proxy/src/main/java/net/sf/freehost3270/proxy/Proxy.java
+++ freehost3270/proxy/src/main/java/net/sf/freehost3270/proxy/Proxy.java
@@ -19,7 +19,6 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-
 package net.sf.freehost3270.proxy;
 
 import java.io.IOException;
@@ -30,112 +29,172 @@ import java.net.Socket;
 import java.util.Vector;
 import java.util.logging.Logger;
 
-
 /**
- * This class provides the mechanism by which host and client (applet) are
- * able to communicate.
- *
+ * This class provides the mechanism by which host and client (applet) are able
+ * to communicate.
+ * 
  * @since 0.1
  */
 public class Proxy implements ConnectionMonitor, Runnable {
-    private static final Logger log = Logger.getLogger(Proxy.class.getName());
+	private static final Logger log = Logger.getLogger(Proxy.class.getName());
 
-    /** The socket used for listening for client requests. */
-    protected ServerSocket mainSocket = null;
+	/** The socket used for listening for client requests. */
+	protected ServerSocket mainSocket = null;
 
-    /** The vector of active connections. */
-    protected Vector connections = null;
+	/** The vector of active connections. */
+	protected Vector connections = null;
 
-    /** Encryption true/false. */
-    boolean encrypt;
+	/** Encryption true/false. */
+	boolean encrypt;
 
-    /**
-     * Instantiates a new FreeHost3270 Proxy.
-     *
-     * @param portNumber integer representing the port on which to listen for
-     *        clients.
-     * @param encryption default encryption on or off(t/f).
-     *
-     * @throws IOException DOCUMENT ME!
-     */
-    public Proxy(int portNumber, boolean encryption) throws IOException {
-        log.info("starting proxy at port: " + portNumber);
-        connections = new Vector();
-        mainSocket = new ServerSocket(portNumber);
-        encrypt = encryption;
-    }
+	/**
+	 * Instantiates a new FreeHost3270 Proxy.
+	 * 
+	 * @param portNumber
+	 *            integer representing the port on which to listen for clients.
+	 * @param encryption
+	 *            default encryption on or off(t/f).
+	 * 
+	 * @throws IOException
+	 *             DOCUMENT ME!
+	 */
+	public Proxy(int portNumber, boolean encryption) throws IOException {
+		log.info("starting proxy at port: " + portNumber);
+		connections = new Vector();
+		mainSocket = new ServerSocket(portNumber);
+		encrypt = encryption;
+	}
 
-    /**
-     * Adds a Connection object to the connections Vector.
-     *
-     * @param c The connection object to add.
-     */
-    public void addConnection(Connection c) {
-        synchronized (connections) {
-            connections.addElement(c);
-        }
+	/**
+	 * Adds a Connection object to the connections Vector.
+	 * 
+	 * @param c
+	 *            The connection object to add.
+	 */
+	public void addConnection(Connection c) {
+		synchronized (connections) {
+			connections.addElement(c);
+		}
 
-        log.info("new Session from: " + c.getSrcHost() + "-->" +
-            c.getDestHost());
-    }
+		log.info("new Session from: " + c.getSrcHost() + "-->"
+				+ c.getDestHost());
+	}
 
-    /**
-     * Empty method. Satisfies implementation of the Connection Monitor
-     * interface.
-     *
-     * @param c DOCUMENT ME!
-     */
-    public void attemptingConnection(Connection c) {
-        log.finest("attempting connection");
-    }
+	/**
+	 * Empty method. Satisfies implementation of the Connection Monitor
+	 * interface.
+	 * 
+	 * @param c
+	 *            DOCUMENT ME!
+	 */
+	public void attemptingConnection(Connection c) {
+		log.finest("attempting connection");
+	}
 
-    /**
-     * Reports connection error on the proxy log with <code>SEVERE</code>
-     * rating.
-     *
-     * @param c DOCUMENT ME!
-     * @param errMsg DOCUMENT ME!
-     */
-    public void connectionError(Connection c, String errMsg) {
-        log.severe("error reported for connection " + c + ": " + errMsg);
-    }
+	/**
+	 * Reports connection error on the proxy log with <code>SEVERE</code>
+	 * rating.
+	 * 
+	 * @param c
+	 *            DOCUMENT ME!
+	 * @param errMsg
+	 *            DOCUMENT ME!
+	 */
+	public void connectionError(Connection c, String errMsg) {
+		log.severe("error reported for connection " + c + ": " + errMsg);
+	}
 
-    /**
-     * Removes a Connection object from the connections Vector.
-     *
-     * @param c The connection object to remove.
-     */
-    public void removeConnection(Connection c) {
-        synchronized (connections) {
-            connections.removeElement(c);
-        }
+	/**
+	 * Removes a Connection object from the connections Vector.
+	 * 
+	 * @param c
+	 *            The connection object to remove.
+	 */
+	public void removeConnection(Connection c) {
+		synchronized (connections) {
+			connections.removeElement(c);
+		}
 
-        log.info("Session Closed for: " + c.getSrcHost() + "-->" +
-            c.getDestHost());
-    }
+		log.info("Session Closed for: " + c.getSrcHost() + "-->"
+				+ c.getDestHost());
+	}
 
-    /**
-     * Starts the thread. This is designed to use up as little overhead as
-     * possible by spawining the Connection object (and it's corresponding
-     * threads) immeadiately.
-     */
-    public void run() {
-        log.info("running proxy thread");
+	/**
+	 * Starts the thread. This is designed to use up as little overhead as
+	 * possible by spawning the Connection object (and it's corresponding
+	 * threads) immediately.
+	 */
+	public void run() {
+		log.info("running proxy thread");
 
-        while (!Thread.currentThread().isInterrupted()) {
-            try {
-                log.fine("waiting for new connections...");
+		while (!Thread.currentThread().isInterrupted()) {
+			try {
+				log.fine("waiting for new connections...");
 
-                Socket newSocket = mainSocket.accept();
-                log.fine("got new connection");
-                new Connection(newSocket, (ConnectionMonitor) this, encrypt);
-                log.fine("created Connection successful...");
-            } catch (IOException e) {
-                e.printStackTrace();
-                log.severe(e.getMessage());
-            }
-        }
+				Socket newSocket = mainSocket.accept();
+				log.fine("got new connection");
+				new Connection(newSocket, (ConnectionMonitor) this, encrypt);
+				log.fine("created Connection successful...");
+			} catch (IOException e) {
+				e.printStackTrace();
+				log.severe(e.getMessage());
+			}
+		}
 
-        log.info("exiting from the proxy wait connections loop");
-    }
+		log.info("exiting from the proxy wait connections loop");
+	}
+
+	public static void main(String[] args) throws IOException {
+		if (args.length == 0) {
+			System.err.println("The command line options are:");
+			System.err.println("-port port - The port to listen to");
+			System.err
+					.println("-e - optional, if present then encrypt the connections using SSL");
+			System.exit(1);
+		}
+
+		int port = parsePort(args);
+
+		if (port == -1) {
+			log.severe("Port is a required option");
+		}
+		boolean encrypt = parseEncription(args);
+		Proxy prox = new Proxy(port, encrypt);
+		prox.run();
+		
+		
+	}
+
+	private static boolean parseEncription(String[] args) {
+		for (int i = 0; i < args.length; i++) 
+			if (args[i].equals("-e"))
+				return true;
+		return false;
+	}
+
+	private static int parsePort(String[] args) {
+		int port = -1;
+		String portString = null;
+		for (int i = 0; i < args.length; i++) {
+			if (args[i].equals("-port")) {
+				try {
+					portString = args[i + 1];
+					port = Integer.parseInt(portString);
+					return port;
+				} catch (ArrayIndexOutOfBoundsException e) {
+					log
+							.severe("We got the -port option, but no port was specified");
+					throw e;
+				} catch (NumberFormatException e) {
+					log.severe("The port value specified (" + portString
+							+ ") is not a valid integer.");
+					throw e;
+				}
+
+			}
+			
+
+		}
+		return port;
+	}
 }
